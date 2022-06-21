6c246d8adda8d5e14cdb7755da5207b1235c6e0f
FSM\lml6626 <laurette.mhlanga@northwestern.edu>
2022-06-21 17:48:09 -0500

    emod tials

---
+++ construct_migration_files.py
@@ -0,0 +1,37 @@
+
+import os
+import json
+import numpy as np
+import sys
+
+from input_file_generation.convert_txt_to_bin import convert_txt_to_bin
+#C:\Users\lml6626\Documents\urban_malaria\emod_trials\input\Ghana
+user_path =os.path.expanduser("~")
+home_path = os.path.join(user_path, 'Documents', 'urban_malaria')
+data_path = os.path.join(home_path, 'emod_trials', 'input')
+project_path = os.path.join(data_path, 'Ghana')
+
+demo_fname = os.path.join(data_path, 'Ghana', 'Ghana_demographics.json')
+
+with open(demo_fname) as fin:
+    demo = json.loads(fin.read())
+id_reference = demo['Metadata']['IdReference']
+
+#C:\Users\lml6626\Documents\urban_malaria\emod_trials\input
+mig_type = 'LOCAL'
+convert_txt_to_bin(os.path.join(data_path,'local_migration.csv'),
+                   'local_migration.bin',
+                    mig_type = 'LOCAL',
+                    id_reference = id_reference)
+
+cb.update_params({
+        # Migration
+        'Migration_Model': 'FIXED_RATE_MIGRATION', # turn on human migration
+        'Migration_Pattern': 'SINGLE_ROUND_TRIPS', # human trips are round trips (see documentation for other options)
+
+        'Enable_Local_Migration': 1,               # turn on Local human migration
+        'Local_Migration_Roundtrip_Duration': 5,   # Local trips last 5 days can edit to this to 14 days
+        'Local_Migration_Roundtrip_Probability': 1,# traveler returns home in 100% of Local trips
+        'x_Local_Migration': 0.02,                 # Scale factor used to fix the average # of trips per person, per year.
+        'Local_Migration_Filename': 'local_migration.bin' # path to migration file
+    })
